c9827c96ca53f3f0eafb86f37c0f288efe580527
Florian Geckeler <fgeck@Florians-Air.fritz.box>
2022-12-24 16:56:41 +0100

    Initial Commit

---
+++ src/main/kotlin/com/fgeck/surfbuddies/SurfbuddiesApplication.kt
@@ -0,0 +1,11 @@
+package com.fgeck.surfbuddies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SurfbuddiesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SurfbuddiesApplication>(*args)
+}
